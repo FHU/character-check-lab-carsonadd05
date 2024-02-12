538668e02ee6080c700b161f6f78ae93a5d80f67
carsonadd05 <157738396+carsonadd05@users.noreply.github.com>
2024-02-12 14:00:38 -0600

    main
    test

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 #Remove pass and complete the code
 def check_character(word, index):
-   pass
+    if index < 0 or index >= len(word):
+        return "Index out of range"
+    
+    char = word[index]
+    if char.isalpha():
+        return "letter"
+    elif char.isdigit():
+        return "digit"
+    elif char.isspace():
+        return "white space"
+    else:
+        return "unknown"
 
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
